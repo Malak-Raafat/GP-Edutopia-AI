9df0e16a14e5b75f0d7a56b4e7e22070e839462e
Habiba Alaa <113467065+haabiba-alaa@users.noreply.github.com>
2025-06-19 13:24:31 +0300

    Delete Simple Agent directory

--- Simple Agent/tools.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.tools import WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
-
-wikipedia_tool = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
-
-print(wikipedia_tool.name) 
-print(wikipedia_tool.description)
-
-print(wikipedia_tool.run("Trump"))
